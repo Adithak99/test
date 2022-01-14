f7f0c30d82676921c3f808effb57105c20af6ecf
Adithak99 <95956891+Adithak99@users.noreply.github.com>
2022-01-14 12:29:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -82,7 +82,7 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🎖 Main Channel 🎖', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
